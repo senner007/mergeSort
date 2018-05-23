7dd07c8cc78533f5fc8f34a0b788241b9baebb19
senner007 <nielshtg@hotmail.com>
2018-05-23 21:45:07 +0200

    log logic

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function merge(bina, binb) {
     var count2 = 0;
     var binaLength = bina.length;
     var binbLength = binb.length;
-
+   
     while (newArr.length < (binaLength + binbLength)) {
 
         while (bina[count1] <= binb[count2] || count2 === binbLength) {
@@ -19,12 +19,16 @@ function merge(bina, binb) {
         }
 
     }
+    console.log("(" + (i++) + ")" + "MERGE : bina: " + bina + " binb: " + binb + " into: " + newArr);
+
     return newArr;
 }
 
-function mergeSort(arr) {
+function mergeSort(arr, side) {
+    
     var arrLength = arr.length;
     if (arrLength < 3) {
+        console.log("(" + (i++) + ")" + "mSort (sorting): " + arr + " -- " + side);
         if (arrLength > 1) {
             if (arr[0] > arr[1]) {
                 return [arr[1], arr[0]]
@@ -35,33 +39,30 @@ function mergeSort(arr) {
             return arr
         }
     } else {
+        console.log("(" + (i++) + ")" + "mSort (call self): " + arr + " -- " + side);
         var half_length = Math.ceil(arrLength / 2);
-        var leftSide = arr.slice(0, half_length);
-        var rightside = arr.slice(half_length, arrLength);
+        var left = arr.slice(0, half_length);
+        var right = arr.slice(half_length, arrLength);
         return merge(
-            mergeSort(leftSide),
-            mergeSort(rightside)
+            mergeSort(left, "left"),
+            mergeSort(right, "right")
         )
     }
 }
 
 
-var arr = Array.from({
-    length: 2000000
-}, () => Math.floor(Math.random() * 100));
-//console.log(arr)
+var i = 1;
+var arr = [1, 6, 4, 5, 3, 2 ];
 
-//var arr = [8, 25, 12, 7, 6, 3, 13, 10, 2, 14];
-console.time("f")
-var sorted = arr.slice(0).sort(function (a, b) {
-    return a - b
-});
-console.timeEnd("f")
-console.time("f2")
-var newArr = mergeSort(
-    arr
-);
-console.timeEnd("f2")
-// console.log(sorted)
-// console.log(newArr)
-console.log(sorted.join('') === newArr.join(''))
+var newArr = mergeSort(arr, "");
+
+// (1)mSort(call self): 1, 6, 4, 5, 3, 2 --
+// (2)mSort(call self): 1, 6, 4 -- left
+// (3)mSort(sorting): 1, 6 -- left
+// (4)mSort(sorting): 4 -- right
+// (5)MERGE: bina: 1, 6 binb: 4 into: 1, 4, 6
+// (6)mSort(call self): 5, 3, 2 -- right
+// (7)mSort(sorting): 5, 3 -- left
+// (8)mSort(sorting): 2 -- right
+// (9)MERGE: bina: 3, 5 binb: 2 into: 2, 3, 5
+// (10)MERGE: bina: 1, 4, 6 binb: 2, 3, 5 into: 1, 2, 3, 4, 5, 6
